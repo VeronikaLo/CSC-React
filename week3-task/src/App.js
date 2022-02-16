3511487f5f16d106fcd3b0804965eff797c7b5dc
Veronika L <verokipri@yahoo.com>
2022-02-16 10:32:16 +0100

    added 2.picture

--- week3-task/src/App.js
+++ week3-task/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import girl from './assets/girl.jpg';
 import Activities from './Activities';
+import cat from './assets/cat.jpg';
 import Likes from './Likes';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <img src ={ girl } width="300px" alt="girl"/>
       <h1>My activities</h1>
       <Activities/>
+      <img src ={ cat } width="200px" alt="cat" className='image'/>
       {/*<Likes/>*/}
     </div>
   );
